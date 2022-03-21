be52d2c110837ad4141471028b2103ee0ed1d3a7
angiekimm <eungg22@gmail.com>
2022-03-21 18:52:40 +0900

    14/ 신규 아이디 추천

--- Programmers/level1/72410.py
+++ Programmers/level1/72410.py
@@ -3,26 +3,24 @@ import re
 # https://programmers.co.kr/learn/courses/30/lessons/72410?language=python3
 
 def solution(new_id):
-    answer = ''
     # 1단계
     new_id = new_id.lower()
+    # 2단계
+    new_id = re.sub("[^-_.a-z0-9]", "" "", new_id)
     # 3단계
-    i=0
-    while 1:
-        if i == len(new_id)-1:
-            break
-        if new_id[i] == new_id[i+1]:
-            str1 = new_id[0:i]
-            str2 = new_id[i+1:]
-            new_id = str1 + str2
-            i=0
-        else:
-            i += 1
-
+    new_id = re.sub("[.]{2,}", ".", new_id)
     # 4단계
-    if(new_id[0]=='.'):
-        new_id[1:]
-    if(new_id[len(new_id)-1]=='.'):
-        new_id[:len(new_id)-1]
+    new_id = re.sub("^[.]|[.]$", "", new_id)
+    # 5단계
+    if(len(new_id)==0):
+        new_id = "a"
+    # 6단계
+    if(len(new_id) >= 16):
+        new_id = new_id[:15]
+        new_id = re.sub("[.]$", "", new_id)
+    # 7단계
+    if(len(new_id) <= 2):
+        while(len(new_id) < 3):
+            new_id += new_id[-1]
     print(new_id)
-    return answer
+    return new_id
