f918ca49a8a0912f4e46e8a7ecb6f1784dbd8332
angiekimm <eungg22@gmail.com>
2022-03-28 14:31:29 +0900

    보물

---
+++ BaekJoon/1026.py
@@ -0,0 +1,18 @@
+# 보물
+
+def treasure(x):
+    N = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    
+    S = 0
+    A.sort()
+
+    for i in range(N):
+        S += A[i] * max(B)
+        B.remove(max(B))
+
+    # 결과
+    print(S)
+
+treasure(0)
