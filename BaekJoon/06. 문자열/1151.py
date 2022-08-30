18d95dac4255d644dedf364e4c07a62bb5a791d4
angiekimm <eungg22@gmail.com>
2022-08-30 16:56:47 +0900

    단어의 개수

--- BaekJoon/06. 문자열/1151.py
+++ BaekJoon/06. 문자열/1151.py
@@ -2,5 +2,5 @@
 import sys
 # split() - 공백의 갯수와 상관없이 split
 # split(" ") - 공백 하나를 기준으로 split, 공백이 두 개면 공백 하나가 리스트에 들어옴
-words = sys.stdin.readline().strip().split(" ")
+words = sys.stdin.readline().strip().split()
 print(len(words))
