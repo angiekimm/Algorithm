7513b78b32277d88dad2ce53c4e3fa0be62cce99
angiekimm <eungg22@gmail.com>
2022-08-30 12:11:59 +0900

    단어 공부_update

--- BaekJoon/06. 문자열/1157.py
+++ BaekJoon/06. 문자열/1157.py
@@ -1,5 +1,7 @@
 # 단어 공부
+"""
 import sys
+
 word = list(sys.stdin.readline().strip().upper())
 counter = {}
 
@@ -23,3 +25,18 @@ else:
         print('?')
     else:
         print(sorted_counter[0][0])
+"""
+import sys
+word = sys.stdin.readline().strip().upper()
+set_word = list(set(word))
+
+cnt_list = []
+for i in set_word:
+    cnt = word.count(i)
+    cnt_list.append(cnt)
+
+if cnt_list.count(max(cnt_list)) > 1:
+    print('?')
+else:
+    max_index = cnt_list.index(max(cnt_list))
+    print(set_word[max_index])
