2368d27b84eafb722e88fe4f1d4a334885899711
angiekimm <eungg22@gmail.com>
2022-08-30 21:36:05 +0900

    상수

--- BaekJoon/06. 문자열/2908.py
+++ BaekJoon/06. 문자열/2908.py
@@ -1,1 +1,23 @@
 # 상수
+import sys
+
+# solution 1
+numbers = sys.stdin.readline().strip().split()
+
+def reverse_number(numbers):
+    answer = ''
+    for i in reversed(numbers):
+        answer += i
+    return int(answer)
+
+reversed_number1 = reverse_number(numbers[0])
+reversed_number2 = reverse_number(numbers[1])
+
+print(reversed_number1) if reversed_number1 > reversed_number2 else print(
+    reversed_number2)
+
+# solution 2
+a, b = sys.stdin.readline().strip().split()
+new_a = int(a[2]+a[1]+a[0])
+new_b = int(b[2]+b[1]+b[0])
+print(max(new_a, new_b))
