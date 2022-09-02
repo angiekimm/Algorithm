c3f80a67f5a9dbdbb280164157587b2deaba75c5
angiekimm <eungg22@gmail.com>
2022-09-02 14:23:11 +0900

    상수

--- BaekJoon/06. 문자열/2908.py
+++ BaekJoon/06. 문자열/2908.py
@@ -10,8 +10,8 @@ def reverse_number(numbers):
         answer += i
     return int(answer)
 
-reversed_number1 = reverse_number(numbers[0])
-reversed_number2 = reverse_number(numbers[1])
+reversed_number1 = reverse_number(numbers[0]) # 437
+reversed_number2 = reverse_number(numbers[1]) # 398
 
 print(reversed_number1) if reversed_number1 > reversed_number2 else print(
     reversed_number2)
